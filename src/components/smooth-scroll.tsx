147051a57e27a7f5e15e7ad5d4f746ec9796afec
Rene kuhm <renekuhm2@gmail.com>
2025-01-17 00:40:23 -0300

    Corregir importación y configuración de Lenis para scroll suave

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -1,18 +1,16 @@
 'use client'
 
 import { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
 
 export function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
-      touchMultiplier: 2,
+      wheelMultiplier: 1,
+      smoothWheel: true,
+      touchInertiaMultiplier: 2,
+      infinite: false,
     })
 
     function raf(time: number) {
