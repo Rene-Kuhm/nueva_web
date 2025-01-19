9d1bd7903ebd931e76df8c935ba8e7af3f276ee2
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-01-19 20:17:43 -0300

    feat: update Sanity configuration and add studio page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,26 +1,22 @@
-import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
-import { visionTool } from '@sanity/vision';
-import { colorInput } from '@sanity/color-input';
-import { codeInput } from '@sanity/code-input';
-
-import { schemaTypes } from './sanity/schemas';
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { media } from 'sanity-plugin-media'
+import { schemaTypes } from './sanity/schemas' // Corregida la ruta a la ubicación correcta
 
 export default defineConfig({
-  name: 'kuhmdev-blog',
-  title: 'KuhmDev Blog',
-  
-  projectId: 'fuw1p4ub',
-  dataset: 'production',
-  
+  name: 'default',
+  title: 'Nueva Web',
+  basePath: '/studio',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: '2023-01-01',
   plugins: [
     deskTool(),
     visionTool(),
-    colorInput(),
-    codeInput()
+    media(),
   ],
-  
   schema: {
-    types: schemaTypes
-  },
-});
+    types: schemaTypes,
+  }
+})
