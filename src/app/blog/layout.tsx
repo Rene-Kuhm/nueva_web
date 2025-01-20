1d2fa04dd8663195e5ce038263600408cd8f30e9
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-01-20 03:11:28 -0300

    refactor: simplify BlogLayout component by removing unnecessary elements

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import ScrollToTop from '@/components/scroll-to-top';
-import Navbar from '@/components/navbar';
+
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="blog-layout">
-      <Navbar />
-      {children}
-      <ScrollToTop />
-    </div>
-  );
+  return <div className="blog-layout">{children}</div>;
 }
