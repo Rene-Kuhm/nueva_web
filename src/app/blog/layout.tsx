4d14de8328b7e81f376aa4f6cfbff1c94da11385
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-01-19 23:12:35 -0300

    refactor: simplify BlogLayout component by returning children directly

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-
-
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <div className="flex-1">
-
-      {children}
-
-    </div>
-  )
+  return children;
 }
