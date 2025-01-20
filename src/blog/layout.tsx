3a7b34aa01f9672420145e103e87d8f87f842ad6
René Kuhm <157995252+Rene-Kuhm@users.noreply.github.com>
2025-01-19 23:23:18 -0300

    refactor: enhance BlogLayout by adding ThemeProvider, Navbar, Footer, and ScrollToTop components

--- src/blog/layout.tsx
+++ src/blog/layout.tsx
@@ -1,9 +1,24 @@
-'use client';
+import { type ReactNode } from 'react'
+import { ThemeProvider } from 'next-themes'
+import Navbar from '@/components/navbar'
+import {Footer} from '@/components/footer'
+import ScrollToTop from '@/components/scroll-to-top'
 
 export default function BlogLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
-  return children;
+  return (
+    <ThemeProvider attribute="class">
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer />
+        <ScrollToTop />
+      </div>
+    </ThemeProvider>
+  )
 }
